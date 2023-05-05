dfc22e1a721233d4731ba2d20c15f4ced45dbeb6
Mistral46 <reyleal@gmail.com>
2023-05-05 10:25:18 -0400

    Test1DSS

--- routes/routes.js
+++ routes/routes.js
@@ -2,12 +2,8 @@ const express = require('express')
 const router = express.Router()
 const main = require('../controllers/MainController')
 
-router.get('/test',main.test)
-router.post('/postData',main.postData)
-router.get('/index',main.index)
-router.post('/enviarDatos',main.enviarDatos)
-router.get('/ejercicio',main.ejercicio)
-router.get('/ejercicio',main.ejercicio)
-router.post('/recibeDataEjercicio',main.recibeDataEjercicio)
+router.get('/vista_ejercicio',main.vista_ejercicio)
+router.post('/recibe',main.recibe)
 
 module.exports = router
+
