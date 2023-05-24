930b312c7ca253ea32d243b6817df847c58c73f0
Mistral46 <reyleal@gmail.com>
2023-05-23 22:06:57 -0400

    23052023

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -5,10 +5,11 @@ const loginForm = (request,response) => {
     response.render('login')
 }
 const doLogin = async (request,response)=>{
+    console.log(request.body)
      myConnection.query(
-        `select * from users where username = "${request.body.username}"
-         and password = "${request.body.password}"`,
+        `select * from users where username = "${request.body.username}"and password = "${request.body.password}"`,
         function(err, results) {
+            console.log(results)
           if(results){
             response.json({message:"Login Exitoso"})
         }else{
